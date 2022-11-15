7769319b9abfda1505f7002b14cd74685dcc2831
habasm <habasm365@gmail.com>
2022-11-15 17:58:58 +0300

    eslint error fiz-1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // Mobile menu functionality
-export const menuIconMobile = document.querySelector('.navbar__hamb');
+export const menuIconMobile = document.querySelector(".navbar__hamb");
 const menu = document.querySelector('.navbar__menu');
 export const menuLinks = document.querySelectorAll('.navbar__menu__link');
 
